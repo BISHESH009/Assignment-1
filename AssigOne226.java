8279e0cc4f283ee601959189425edd4a53dfc49b
BISHESH009 <bisheshk@utas.edu.au>
2026-08-04 17:58:37 +0545

    assignmant

---
+++ AssigOne226.java
@@ -0,0 +1,10 @@
+public class AssigOne226
+{
+    public static void main(String[] args)
+    {
+      PeriodicTable table = new PeriodicTable();   
+    System.out.println("Periodic Table Printer"); 
+    table.printTable();
+    table.printGroups();
+    }
+}
